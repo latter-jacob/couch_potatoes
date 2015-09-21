bcb2c5f6622809cc5267d19e5198106f5ba0f5b0
margonaut <margaret.morgane@gmail.com>
2015-09-21 13:48:11 -0400

    shows migration and factory girl setup

---
+++ spec/features/user_views_index_page_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'user ', %(
+As a use, when I visit the root page
+I want to see a list of shows to review.
+So that I can decide which one to judge.
+
+  Acceptance Criteria
+  [ ] - I must see the name of each show
+
+) do
+
+  scenario 'user views all shows' do
+    # show_1 = Show.create(
+    #   name: "Doug",
+    #   url: "https://www.youtube.com/watch?v=JR5zFiIxqSs",
+    #   debut_year: "1991",
+    #   end_year: "1999",
+    #   genre: "cartoon"
+    # )
+
+    show_1 = FactoryGirl.create(:show, title: "Doug")
+
+    visit root_path
+
+    expect(page).to have_content(show_1.title)
+    expect(page).to have_content("Doug")
+  end
+end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -7,4 +7,12 @@ FactoryGirl.define do
     password_confirmation 'password'
   end
 
+  factory :show do
+    sequence(:title) {|n| "show #{n}"}
+    sequence(:url) {|n| "http://www.#{n}show.com"}
+    start_year "1991"
+    end_year "1999"
+    genre "cartoon"
+    end
+
 end
