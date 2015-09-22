231416b6692a9ddb7e9c87660c1d56604c41fbdd
Jeremy <jgertz26@gmail.com>
2015-09-22 17:11:00 -0400

    fixed that one thing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,6 @@ class ReviewsController < ApplicationController
   def create
     authenticate_user!
     @program = Program.find(params[:program_id])
-    @user = current_user
     @review = Review.new(review_params)
 
     if @review.save
@@ -19,7 +18,7 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:rating, :body).merge(
       program: @program,
-      user: @user
+      user: current_user
       )
   end
 end
