474e0e2440b4fde3dbadbea85ee6bb0a3dc37fe2
Jeremy <jgertz26@gmail.com>
2015-09-22 10:53:01 -0400

    hound errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,11 +4,13 @@ class Review < ActiveRecord::Base
 
   validates :user, uniqueness: { scope: :program,
     message: "You may only review a program once." }
-    
+
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: 1..5, message: "Must be between 1 - 5" }
-  validates :body, length: {maximum: 200}, allow_blank: true
+  validates :rating, inclusion: { in: 1..5,
+             message: "Must be between 1 - 5" }
+
+  validates :body, length: { maximum: 200 }, allow_blank: true
   validates :score, presence: true
   validates :score, numericality: { only_integer: true }
 
--- spec/models/programs_spec.rb
+++ spec/models/programs_spec.rb
@@ -6,7 +6,7 @@ describe Program do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:url) }
 
-  subject{ FactoryGirl.create(:program) }
+  subject { FactoryGirl.create(:program) }
   it { should validate_uniqueness_of(:title) }
   it { should validate_uniqueness_of(:url) }
 end
