120e093dfc141b6dc657e81ca24ce2c2ac400423
Jeremy <jgertz26@gmail.com>
2015-09-22 10:56:32 -0400

    more hound errorsssss

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,10 @@ class Review < ActiveRecord::Base
   belongs_to :program
   belongs_to :user
 
-  validates :user, uniqueness: { scope: :program,
-                                 message: "You may only review a program once." }
+  validates :user, uniqueness: {
+    scope: :program,
+    message: "You may only review a program once."
+  }
 
   validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }
