493e155de89b936498386277b2d36de4910e6e27
margonaut <margaret.morgane@gmail.com>
2015-09-28 11:44:46 -0400

    JavaScript hound fixes

--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,4 +1,4 @@
-var vote_classes = function(thing) {
+var voteClasses = function(thing) {
   if (thing.id == "upvote") {
     $(thing).attr("id", "upvoted");
     $(thing.nextElementSibling).attr("id", "downvote");
@@ -12,20 +12,20 @@ var vote_classes = function(thing) {
     $(thing).attr("id", "downvote");
     $(thing.previousElementSibling).attr("id", "upvote");
   }
-}
+};
 
 $(".vote").on("click", function() {
-  var vote = this.id
-  var review_id = this.parentElement.id;
-  vote_classes(this);
+  var vote = this.id;
+  var reviewId = this.parentElement.id;
+  voteClasses(this);
   var $t = $(this);
   $.ajax({
     method: "POST",
-    url: ("/reviews/" + review_id + "/votes"),
-    data: { review_id: review_id, vote: vote },
+    url: ("/reviews/" + reviewId + "/votes"),
+    data: { review_id: reviewId, vote: vote },
     dataType: "json"
   })
   .done(function(data){
-    $($t.siblings('.score')[0]).text(data.score);
+    $($t.siblings(".score")[0]).text(data.score);
   });
 });
