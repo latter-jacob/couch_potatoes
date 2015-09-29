337dff141b910d5e4c1b28f4cbd7f0075aa7e198
margonaut <margaret.morgane@gmail.com>
2015-09-29 14:36:50 -0400

    Added Random program button method

--- app/models/program.rb
+++ app/models/program.rb
@@ -8,4 +8,18 @@ class Program < ActiveRecord::Base
   validates :url, presence: true
   validates :url, uniqueness: true
 
+  def self.get_random_program(user)
+    if user.nil?
+      programs = Program.all
+      programs.sample
+    else
+      user_reviews = user.reviews
+      excluded_ids  = []
+      user_reviews.each do |review|
+        excluded_ids << review.program_id
+      end
+      unrated_programs = Program.where('id NOT IN (?)', excluded_ids)
+      unrated_programs.sample
+    end
+  end
 end
