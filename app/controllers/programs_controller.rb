45a35837d8a947f81230ce221e1729f3f778be8c
margonaut <margaret.morgane@gmail.com>
2015-10-01 17:31:44 -0400

    Refactored sidebar

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -19,7 +19,7 @@ class ProgramsController < ApplicationController
     @program = Program.find(params[:id])
     @reviews = @program.reviews.order_by_vote.page params[:page]
     @review = Review.new
-    @programs = [Program.get_random_program(current_user), Program.get_random_program(current_user), Program.get_random_program(current_user)]
+    @programs = Program.get_sidebar
   end
 
   def create
