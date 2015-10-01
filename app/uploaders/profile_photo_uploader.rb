ecff41e47550f65cc031f7d4c08a5c7a77e55b47
margonaut <margaret.morgane@gmail.com>
2015-10-01 17:35:51 -0400

    Merge conflict?

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class ProfilePhotoUploader < CarrierWave::Uploader::Base
-  if Rails.env.production?
+  if Rails.env.production? || Rails.env.development?
     storage :fog
   else
     storage :file
